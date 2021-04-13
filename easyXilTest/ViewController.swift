be22de475251017244bd1d68719a410ee620c9c4
chenzhizs <chenzhi@qq.com>
2021-04-13 16:15:54 +0900

    按照视频教程完成了

--- easyXilTest/ViewController.swift
+++ easyXilTest/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var firstCustomView: CustomView!
+    @IBOutlet private weak var secondCustomView: CustomView!
+    @IBOutlet private weak var thirdCustomView: CustomView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.firstCustomView.configureView(title: "First Title")
+        self.secondCustomView.configureView(title: "Second Title")
+        self.thirdCustomView.configureView(title: "Third Title")
     }
 
 
